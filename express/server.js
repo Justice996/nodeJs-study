9b849fda27daad977fdcd46353be4f02d196906b
Sherrychen1 <1393080039@qq.com>
2019-12-01 22:49:54 +0800

    experss处理请求

--- express/server.js
+++ express/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
-const  expressStatic = require('express-static')
+const  expressStatic = require('express-static');
+const bodyParser = require('body-parser');
 //创建服务
 var server = express();
 
 // res req 非侵入式 (增强)保留了原生的功能,又新增了其他的功能(例如send)
 
+server.use(bodyParser.urlencoded({
+   extended : true,   //扩展模式
+   limit :    2*1024*1024     //限制2M
+}));
+
 //三种方法处理请求 get post use  ('/',function(){})
-server.use('/a.html', (req,res)=>{
+server.use('/', (req,res)=>{
+   console.log(req.body);//POST
+   // console.log(req.query);//GET
    res.send({a:1,b:2});
 //    res.write({a:1,b:2}); 会报错
    res.end();
 });
 server.listen(3000);
+
+//req.query get
+//req.body post
