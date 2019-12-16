19889d09b7413fefd3094e72c88a3e9c1a16e613
Sherrychen1 <1393080039@qq.com>
2019-12-16 22:58:36 +0800

    新项目

---
+++ learn/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const static = require("express-static");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
+const route = require("express-route");
+const consolidate = require("consolidate");
+const mysql = require('mysql');
+const multer = require('multer');
+
+var server = express();
+server.listen(3000);
+
+
+//1.获取请求数据
+
+//2.cookie  和session
+
+//3.模板
+
+
+//4.route
+
+
+//5.default : static
